8bb7660c9d7215081952dc5d0923d352275cf6cb
Douglas Peter <154028589+PeterDoouglass@users.noreply.github.com>
2023-12-17 01:41:21 +0200

    Create best protocol.js

---
+++ best protocol.js
@@ -0,0 +1,25 @@
+    "exception_info": {
+      "type": "string, type of the exception logged in the event.",
+      "message": "string, exception message for the event."
+    },
+// searching best protocol
+{
+    "version": "1.0",
+    "subject": "string, description of the new or reopened error group.",
+    "group_info": {
+      "project_id": "string, project that owns the error group.",
+      "detail_link": "string, link to the Error Reporting Details page for the error group."
+    },
+
+const createDiscordMessage = (error) => {
+  var embeds = []
+  let message = {
+    "text": "GCP Alerts",
+    "blocks": [
+      {}
+      }{
+
+      message.embeds = embeds;
+console.log(message)
+return message;
+}
